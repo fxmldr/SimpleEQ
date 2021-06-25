d80bff676f07fac89dfbb42a8af71517e36947e8
Nick <kaniushyk.nikita@gmail.com>
2021-06-25 16:27:38 +0300

    Refactored response curve and refactored how peak coefficients are generated

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -24,7 +24,9 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
+juce::AudioProcessorParameter::Listener,
+juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -34,10 +36,27 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+
+    /** Receives a callback when a parameter has been changed.
+
+        IMPORTANT NOTE: This will be called synchronously when a parameter changes, and
+        many audio processors will change their parameter during their audio callback.
+        This means that not only has your handler code got to be completely thread-safe,
+        but it's also got to be VERY fast, and avoid blocking. If you need to handle
+        this event on your message thread, use this callback to trigger an AsyncUpdater
+        or ChangeBroadcaster which you can respond to on the message thread.
+    */
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { };
+
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider 
         peakFreqSlider,
